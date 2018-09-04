aa0132e68c81e0ff648a7b0223beb2d3d84357f9
唐由之 <tangyz@tongc-soft.com>
2018-09-04 16:11:09 +0800

    第四次提交

--- JenkinsTask/src/main/java/com/ppy/MainApplication.java
+++ JenkinsTask/src/main/java/com/ppy/MainApplication.java
@@ -1,11 +1,15 @@
 package com.ppy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
-
+	
+	 private final static Logger logger = LoggerFactory.getLogger(MainApplication.class);
+	 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MainApplication.class, args);
 	}
